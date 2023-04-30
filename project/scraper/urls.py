a1f3ed5eecf76d9c8620ded3b62ce7651447f5ae
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-04-30 11:40:55 +0900

    update: boiler plate for project

---
+++ project/scraper/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+app_name = "scraper"
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # leads to /scraper/3/
+    # by the end of this project, we should label with 
+    # path("<int:line_number>/", views.detail, name="detail")
+    # and pass in the line number to views.detail to call the corresponding data
+    path('3/', views.line_three, name='line_three'),
+]
