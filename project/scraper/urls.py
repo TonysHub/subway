ad199f58aa6afd50dffb8bf71e1a64693eddb7a4
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-04-30 16:51:51 +0900

    update: scraper folder structure

--- project/scraper/urls.py
+++ project/scraper/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # by the end of this project, we should label with 
     # path("<int:line_number>/", views.detail, name="detail")
     # and pass in the line number to views.detail to call the corresponding data
-    path('3/', views.line_three, name='line_three'),
+    path('/3/', views.line_three, name='line_three'),
 ]
--- project/scraper/views.py
+++ project/scraper/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'scraper/index.html')
-# 이동수 스크래퍼
+
 def line_three(request):
     return render(request, 'scraper/line_three.html')
-# 전성현 스크래퍼
 
-# 박주미 스크래퍼
-
-# 명우성 스크래퍼
-
-# 최민수 스크래퍼
