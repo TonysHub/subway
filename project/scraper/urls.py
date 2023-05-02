d79c695a5d5b4c39c7042353dc6399a59b5689b6
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-02 15:03:19 +0900

    lds project directory setup (#12)
    * update: requirements.txt, pip3 install lxml webdriver-manager
    
    * Revert "update: requirements.txt, pip3 install lxml webdriver-manager"
    
    This reverts commit 1b3e56931265c552859cfb2b5fc9f72b5aeb751e.
    
    * update: project structure

--- project/scraper/urls.py
+++ project/scraper/urls.py
@@ -4,9 +4,5 @@ app_name = "scraper"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # leads to /scraper/3/
-    # by the end of this project, we should label with 
-    # path("<int:line_number>/", views.detail, name="detail")
-    # and pass in the line number to views.detail to call the corresponding data
-    path('/3/', views.line_three, name='line_three'),
+    path('<pk>/', views.details, name='details'),
 ]
--- project/scraper/views.py
+++ project/scraper/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import generic
+
+# models 안에 있는 Stations 클래스를 가져옴
+# from .models import Stations
+
 
 def index(request):
     return render(request, 'scraper/index.html')
 
-def line_three(request):
-    return render(request, 'scraper/line_three.html')
+# class IndexView(ListView):
+#     template_name = 'scraper/index.html'
+#     context_object_name = 'all_stations'
 
+#     def get_queryset(self):
+#         # 이 부분은 성현님이 데이터셋 추가하면 구현 가능
+#         return Stations.objects.all()
+
+def details(request):
+    return render(request, 'scraper/details.html')
+
+class DetailView(generic.DetailView):
+    model = Stations
+    template_name = 'scraper/details.html'
