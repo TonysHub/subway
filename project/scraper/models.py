aa9d50e211c76ae23867d46562fc006ec7aba29f
SEONG_HYEON <70497132+Jeon-peng@users.noreply.github.com>
2023-05-02 16:37:30 +0900

    add: add models ( name  : Stations ) (#14)

--- project/scraper/models.py
+++ project/scraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# 나중에 Stations.station은 FK 로 변경
+class Stations (models.Model):
+    station = models.CharField(max_length = 60, primary_key= True)
+    line = models.CharField(max_length = 40)
