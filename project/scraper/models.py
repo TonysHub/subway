86cfe9aadf0d2ed9551891653e7417296b09ee9a
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-03 14:05:06 +0900

    update: 호선 이미지 클릭시, 해당 호선 역명 출력

--- project/scraper/models.py
+++ project/scraper/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
-
-
 
 # 나중에 Stations.station은 FK 로 변경
 class Stations (models.Model):
     station = models.CharField(max_length = 60, primary_key= True)
     line = models.CharField(max_length = 40)
+    slug = models.SlugField(max_length = 60, default= '7호선')
+
+    def save(self, *args, **kwargs):
+        self.slug = self.slug or slugify(self.line)
+        super().save(*args, **kwargs)
