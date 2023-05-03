ad6f87659504528a7f344693bc03836d7c0f8461
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-03 16:23:45 +0900

    update: links to individual stations (#19)

--- project/scraper/urls.py
+++ project/scraper/urls.py
@@ -4,5 +4,6 @@ app_name = "scraper"
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<slug>/', views.line_details, name='line-details')
+    path('<slug>/', views.line_details, name='line-details'),
+    path('<str:line>/<str:station>/', views.station_details, name='station-details')
 ]
