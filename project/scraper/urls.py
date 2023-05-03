9887e06e462dc4dd32b88f3d59b5dec6a7bff340
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-03 23:15:33 +0900

    Lds feature searchbox (#23)
    * feature: searchbox
    
    * update: pk field in views

--- project/scraper/urls.py
+++ project/scraper/urls.py
@@ -4,6 +4,8 @@ app_name = "scraper"
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('searchResult/', views.search_station , name='search-station'),
     path('<slug>/', views.line_details, name='line-details'),
-    path('<str:line>/<str:station>/', views.station_details, name='station-details')
+    path('<str:line>/<str:station>/', views.station_details, name='station-details'),
+
 ]
