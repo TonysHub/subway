0452dc7c40c2aa8150b28c8008fc46eb9b302beb
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-04 14:07:02 +0900

    update: display, links

--- project/scraper/views.py
+++ project/scraper/views.py
@@ -4,15 +4,14 @@ from django.views.generic import ListView
 from django.views import generic
 from django.shortcuts import get_object_or_404
 
-# models 안에 있는 Stations (dummy data) 클래스를 가져옴
 from .models import Stations
 
 class IndexView(ListView):
     template_name = 'scraper/index.html'
-    context_object_name = 'all_stations'
+    context_object_name = 'all_lines'
     
     def get_queryset(self):
-        return Stations.objects.all()
+        return Stations.objects.values('line').distinct()
 
 
 
