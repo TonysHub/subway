3a38861b1df410b0c2a46ffbcd9325c6285591d7
swanim <64563859+swanim@users.noreply.github.com>
2023-05-04 19:54:38 +0900

    add:db_to_df (#31)
    * add:db_to_df
    
    * add:requirements views
    
    * add:requirements

--- project/scraper/views.py
+++ project/scraper/views.py
@@ -1,10 +1,13 @@
-from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import generic
 from django.shortcuts import get_object_or_404
 
-from .models import Stations
+from .models import *
+
+# for plotly
+import plotly.express as px
+import pandas as pd
 
 class IndexView(ListView):
     template_name = 'scraper/index.html'
@@ -15,16 +18,24 @@ class IndexView(ListView):
 
 
 
-
 # 추후에 CBV로 변경
 def line_details(request, slug):
     obj = Stations.objects.filter(line=slug)
     line_name = slug
     return render(request, 'scraper/line_details.html', {'obj': obj, 'line_name': line_name})
 
+
 def station_details(request, line, station):
-    # line 검색 추가해서, 예를 들어 3호선에서 고속터미널역 클릭시, 7호선 고속터미널 정보는 안나옴
+
     obj = get_object_or_404(Stations, station=station, line=line)
+    
+    obj_DailyTraffic = DailyTraffic.objects.filter(station=obj.station, line=obj.line)
+    
+    obj_HourlyTraffic = HourlyTraffic.objects.filter(station=obj.station, line=obj.line) 
+    # df 수정
+    df_subway_traffic_daily = db_to_df(obj_DailyTraffic)
+    df_subway_traffic_month_hourly = db_to_df(obj_HourlyTraffic)
+
     return render(request, 'scraper/station_details.html', {'station': obj})
 
 def search_station(request):
@@ -34,3 +45,12 @@ def search_station(request):
         return render(request, 'scraper/search_station.html', {'stations': stations, 'station_name': query})
     else:
         return render(request, 'scraper/search_station.html', {})
+    
+# date, month 데이터 형식 수정
+def db_to_df(obj):
+    df = pd.DataFrame(obj.values())
+    if 'date' in df.columns:
+        df['date'] = df['date'].dt.date
+    else:
+        df['month'] = df['month'].dt.date
+    return df
