aa33a39b7f3f69e21c46554877088e01ec7edf7a
Jeon-peng <pengping@kakao.com>
2023-05-04 10:57:01 +0900

    update: update models.py verbose_name

--- project/scraper/models.py
+++ project/scraper/models.py
@@ -18,8 +18,8 @@ abstract를 통하여 상속
 """
 class Common(models.Model):
     id = models.AutoField(primary_key=True)
-    station = models.CharField(max_length = 60, null=False, verbose_name="호선명")
-    line = models.CharField(max_length = 20,null=False, verbose_name="키워드")
+    station = models.CharField(max_length = 60, null=False, verbose_name="역 명")
+    line = models.CharField(max_length = 20,null=False, verbose_name="호선명")
     
     
     class Meta:
