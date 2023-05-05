931ee4a20fd7383ebe0547589420ce1e65a3bff4
usiohc <asltn99@gmail.com>
2023-05-05 18:14:03 +0900

    마지막 마지막 커밋

--- project/crontab.py
+++ project/crontab.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def startCrawler():
     print('crontab.py startCrawler', datetime.now())
-    return exec(open('crawler.py').read())
+    return exec(open('daily_process.py').read())
 
 def startTest():
     print('crontab.py startTest', datetime.now())
