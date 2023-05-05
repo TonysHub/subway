993e74fce9aa006b6c01fa7a35b53b7e678fb0df
usiohc <asltn99@gmail.com>
2023-05-05 12:02:57 +0900

    update: hourlytraffic_to_plotly  and station_deatils

--- project/scraper/views.py
+++ project/scraper/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import generic
 from django.shortcuts import get_object_or_404
-
 from .models import *
 
 # for plotly
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
+
 
 class IndexView(ListView):
     template_name = 'scraper/index.html'
@@ -31,17 +31,16 @@ def station_details(request, line, station):
     obj = get_object_or_404(Stations, station=station, line=line)
     
     obj_DailyTraffic = DailyTraffic.objects.filter(station=obj.station, line=obj.line)
-    
     obj_HourlyTraffic = HourlyTraffic.objects.filter(station=obj.station, line=obj.line) 
 
     df_subway_traffic_daily = db_to_df(obj_DailyTraffic)
     df_subway_traffic_month_hourly = db_to_df(obj_HourlyTraffic)
 
     dailytraffic = dailytraffic_to_plotly(df_subway_traffic_daily, line, station)
-    #print(dailytraffic)
-    # hourlytraffic = hourlytraffic_to_plotly(df_subway_traffic_month_hourly, line, station)
+    hourlytraffic = hourlytraffic_to_plotly(df_subway_traffic_month_hourly, line, station)
 
-    return render(request, 'scraper/station_details.html', {'station': obj, 'dailytraffic': dailytraffic})
+    return render(request, 'scraper/station_details.html', {'station': obj, 'dailytraffic':dailytraffic, 'hourlytraffic': hourlytraffic})
+
 
 def search_station(request):
     if request.method == "POST":
@@ -50,26 +49,71 @@ def search_station(request):
         return render(request, 'scraper/search_station.html', {'stations': stations, 'station_name': query})
     else:
         return render(request, 'scraper/search_station.html', {})
-    
-# date, month 데이터 형식 수정
-def db_to_df(obj):
-    df = pd.DataFrame(obj.values())
-    if 'date' in df.columns:
-        df['date'] = df['date'].dt.date
+
+
+def search_station(request):
+    if request.method == "POST":
+        query = request.POST["searched"]
+        stations = Stations.objects.filter(station=query)
+        return render(request, 'scraper/search_station.html', {'stations': stations, 'station_name': query})
     else:
-        df['month'] = df['month'].dt.date
-    return df
+        return render(request, 'scraper/search_station.html', {})
+
 
 def hourlytraffic_to_plotly(df, line, station):
-
+    # 우리가 보고자 하는 Month, 그리고 우리가 보고자 하는 Station을 입력으로 받습니다.
+    # target_month = df['month']
+    target_station = station
+    month_list = df['month'].unique()
     
-    return df
+    # 보여줄 달과 보여줄 역 선택
+    colors = ['red', 'gray', 'steelblue']
+    # target_station = '동대문'
 
+    fig = go.Figure()
+
+    for i, target_month in enumerate(month_list):
+        # 보여줄 역에 해당하는 데이터프레임을 선택
+        print(month_list)
+        filtered_data = df[(df['month']==target_month) & (df['line'] == line)]
+        # filtered_data = df[(df['month'].str.startswith(target_month)) & (df['station'] == target_station)]
+        target_month = str(target_month)[:-3]
+        
+        answer = []
+        hourlist = []
+
+        # in_, out_으로 시작하는 부분부터 
+        for col in filtered_data.columns[3:]:
+            # in이면
+            if col.startswith('in_'): 
+                in_data = filtered_data.iloc[0][col]
+            # out이면 / in-out 번갈아가면서 
+            elif col.startswith('out_'):
+                out_data = filtered_data.iloc[0][col]
+                # 유동인구 고려할때 토탈로
+                total = in_data + out_data
+                answer.append(total)
+
+                hour = col[4:6]
+                hourlist.append(hour)
+
+        color = colors[i]
+        trace_total = go.Scatter(x=hourlist, y=answer, mode='lines+markers', name=target_month, marker=dict(color=color))
+        fig.add_trace(trace_total)
+
+    fig.update_layout(title=f'Hourly 그래프 for {target_station} Station in 3 months',
+                    xaxis_title='시간(Hour)', yaxis_title='역내 유동인구 수',
+                    xaxis=dict(tickangle=0), width=900, height=400)
+
+    line_chart = fig.to_html(full_html=False, include_plotlyjs=False)
+    return line_chart
 
 
 def dailytraffic_to_plotly(df, line, station):
     information = {}
-
+    target_station = station
+    
+    
     for _, row in df.iterrows():
         station = row['station']
         date = row['date']
@@ -86,11 +130,11 @@ def dailytraffic_to_plotly(df, line, station):
     def find_total_passenger(date_data):
         return sum(people_in + people_out for people_in, people_out in date_data.values())
 
-    station_totals = {station: find_total_passenger(date_data) for station, date_data in information.items()}
+    # station_totals = {station: find_total_passenger(date_data) for station, date_data in information.items()}
 
     # 어느역을 기준으로 그래프를 그릴지를 나타내는 변수 target_station
     # 역을 클릭하면 해당 역의 이름이 나오면 될 거 같습니다.
-    target_station = station
+    
 
     answer = []
 
@@ -114,7 +158,18 @@ def dailytraffic_to_plotly(df, line, station):
         answer.append(trace)
 
     fig = go.Figure(data=answer)
-    fig.update_layout(title=f'전체 승객수 in {target_station}역', xaxis_title='Date', yaxis_title='역내 유동인구 수')
+    fig.update_layout(title=f'전체 승객수 in {target_station}역', xaxis_title='Date', yaxis_title='역내 유동인구 수', width=900, height=400)
     fig.update_xaxes(tickangle=0, tickvals=dates_with_day, tickfont=dict(size=10))
+    
     line_chart = fig.to_html(full_html=False, include_plotlyjs=False)
     return line_chart
+
+
+# date, month 데이터 형식 수정
+def db_to_df(obj):
+    df = pd.DataFrame(obj.values())
+    if 'date' in df.columns:
+        df['date'] = df['date'].dt.date
+    else:
+        df['month'] = df['month'].dt.date
+    return df
